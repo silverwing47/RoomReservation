6d17fa2a0d4cb7c2c5edce0930289c410a6dc322
J-alms <77146345+J-alms@users.noreply.github.com>
2022-02-23 19:30:34 +0800

    chgn

--- interior/login.php
+++ interior/login.php
@@ -65,15 +65,15 @@
         <!-- Login Form -->
         <div class="login-reg-panel">
         		<div class="login-info-box">
-        			<h2>Have an account?</h2>
-        			<p>Lorem ipsum dolor sit amet</p>
+        			<h2>Do you have an account?</h2>
+        			<p>Login Here.</p>
         			<label id="label-register" for="log-reg-show">Login</label>
         			<input type="radio" name="active-log-panel" id="log-reg-show"  checked="checked">
         		</div>
 
         		<div class="register-info-box">
         			<h2>Don't have an account?</h2>
-        			<p>Lorem ipsum dolor sit amet</p>
+        			<p>Register now.</p>
         			<label id="label-login" for="log-login-show">Register</label>
         			<input type="radio" name="active-log-panel" id="log-login-show">
         		</div>
