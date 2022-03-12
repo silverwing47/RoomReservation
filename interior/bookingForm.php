b92cdc7f25988abf968dc0c4a5b94d0a250f240a
J-alms <77146345+J-alms@users.noreply.github.com>
2022-03-12 15:40:35 +0800

    toReserve

--- interior/bookingForm.php
+++ interior/bookingForm.php
@@ -4,6 +4,19 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
+
+  session_start();
+
+  $user = $_SESSION["login"];
+
+  $sql = "SELECT * FROM tbl_room";
+  $result = mysqli_query($conn, $sql);
+  $row = $result->fetch_assoc();
+
+
+  // $name_query = "SELECT FROM tbl_client WHERE name='$user' ";
+  //usbonon hahah; to show name on first part sa booking, karun is username pa ang mu show
+
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +30,8 @@
     <!-- Bootstrap CSS -->
   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <!-- Bootstrap JS -->
+
+
   	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
@@ -38,48 +53,89 @@
           min-height: 200px;
           max-width: 100%;
       }
+      body {
+        overflow:auto;
+      }
     </style>
 </head>
 <body>
-    <!-- Table Reservation Form -->
-    <section id="book-a-table" class="book-a-table">
-        <div class="container" data-aos="fade-up">
-            <div class="row">
-    			<div class="col-md-8 offset-md-2 text-center">
-    				<h2 class="text-primary">Re.Srv A Conference Room Now!!</h2>
-    				<p class="mb-5">Select the room you want for your meetings/conferences. Re.Srv Now!</p>
-    			</div>
-    		</div>
 
-            <form action="" method="post" role="form">
-              <div class="form-row">
-                <div class="col-lg-4 col-md-6 form-group">
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name">
+
+      <!-- Table Reservation Form -->
+      <section id="book-a-table" class="book-a-table">
+          <div class="container" data-aos="fade-up">
+              <div class="row">
+      			<div class="col-md-8 offset-md-2 text-center">
+      				<h2 class="text-primary">Re.Srv A Conference Room Now!!</h2>
+      				<p class="mb-5">Select the room you want for your meetings/conferences. Re.Srv Now!</p>
+      			</div>
+      		</div>
+
+              <form action="" method="POST" role="form">
+                <div class="form-row">
+                  <div class="col-lg-4 col-md-6 form-group">
+                      <input type="text" name="name" class="form-control" id="name" placeholder="<?=$user?>" value="<?=$user?>" readonly>
+                      <!-- username lng sa ang ako gi use hahah di ko kamao pa show sa name from tbl_client -->
+                      <!-- di ma input sa  -->
+                  </div>
+                  <div class="col-lg-4 col-md-6 form-group">
+                      <input type="text" class="form-control" name="address" id="address" placeholder="Your Address">
+                  </div>
+                  <div class="col-lg-4 col-md-6 form-group">
+                      <input type="text" class="form-control" name="phone" id="phone" placeholder="Your Phone (11 digit phone number)" value="09">
+                  </div>
+                  <div class="col-lg-4 col-md-6 form-group">
+                      <input type="date" name="date" class="form-control" id="date" placeholder="Date">
+                  </div>
+                  <div class="col-lg-4 col-md-6 form-group">
+                      <!-- <input type="text" class="form-control" name="time" id="time" placeholder="Time"> -->
+                      <select class="form-control" name="timeslot" id="timeslot">
+                        <option hidden value="default">- - - Select Timeslot - - -</option>
+                        <option value='Morning'>Morning</option>
+                        <option value='Afternoon'>Afternoon</option>
+                        <option value='Evening'>Evening</option>
+                      </select>
+                  </div>
+                  <div class="col-lg-4 col-md-6 form-group">
+                      <!-- <input type="text" class="form-control" name="people" id="room" placeholder=""> -->
+                      <select class="form-control" name="roomNumber" id="room">
+                        <option hidden value="default">- - - Select Room - - -</option>
+                        <?php foreach( $result as $row ){
+                            echo "<option value='" . $row['id'] . "'>" . $row['roomNumber'] . "</option>";
+                            }
+                        ?>
+                        <!-- butangan unta ug price after sa roomnumber but di nako ma implement hahaha -->
+                        <!-- kailangan mu connect sa roomtype table hahaha -->
+                      </select>
+                  </div>
                 </div>
-                <div class="col-lg-4 col-md-6 form-group">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email">
-                </div>
-                <div class="col-lg-4 col-md-6 form-group">
-                    <input type="number" class="form-control" name="phone" id="phone" placeholder="Your Phone">
-                </div>
-                <div class="col-lg-4 col-md-6 form-group">
-                    <input type="date" name="date" class="form-control" id="date" placeholder="Date">
-                </div>
-                <div class="col-lg-4 col-md-6 form-group">
-                    <input type="time" class="form-control" name="time" id="time" placeholder="Time">
-                </div>
-                <div class="col-lg-4 col-md-6 form-group">
-                    <input type="number" class="form-control" name="people" id="people" placeholder="No. of people">
-                </div>
-              </div>
-              <div class="form-group">
-                    <textarea class="form-control" name="message" placeholder="Message"></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary float-right mt-3" name="btnReserve">Reserve Now</button>
-            </form>
-        </div>
-    </section>
-    <!-- End Table Reservation Form -->
-    
+                <!-- <div class="form-group">
+                      <textarea class="form-control" name="message" placeholder="Message"></textarea>
+                </div> -->
+                <button type="submit" class="btn btn-primary float-right mt-3" name="btnReserve" onclick="reserved()">Reserve Now</button>
+              </form>
+          </div>
+      </section>
+      <!-- End Table Reservation Form -->
+
 </body>
 </html>
+
+<script type="text/javascript">
+  function reserved() { alert("Reservation Booked Successfully!"); }
+</script>
+
+<?php
+  if(isset($_POST['btnReserve'])){
+      $name=$_POST['name'];
+      $address=$_POST['address'];
+      $phone=$_POST['phone'];
+      $rNum=$_POST['roomNumber'];
+      $date=$_POST['date'];
+      $timeslot=$_POST['timeslot'];
+      // $query="INSERT INTO usertbl (firstName, lastName, email, password, phone) VALUES ('$fname','$lname', '$email',SHA('$pwd'), '$phone')"; //for encryption of password utilize SHA()
+      $query="INSERT INTO tbl_reservation (name, address, phone, room_id, date, timeslot) VALUES ('$name','$address', '$phone','$rNum', '$date', '$timeslot')";
+      mysqli_query($conn,$query);
+      header("Location: bookingForm.php");
+    }
+?>
